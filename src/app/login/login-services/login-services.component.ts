b9a9bc5534221e9b9b09559fe2a3c9dceb519c68
vijendra kumar <itsvijendra@gmail.com>
2017-09-18 23:15:43 +0530

    changes for login service approval

--- src/app/login/login-services/login-services.component.ts
+++ src/app/login/login-services/login-services.component.ts
@@ -15,6 +15,7 @@ export class LoginServicesComponent implements OnInit {
   private LoginDetails;
   private loginService: LoginServices;
   private LoginStatusTypes;
+  private message: string;
   private errorMessage: string;
   private searchtext: string = "";
   constructor(formBuilder: FormBuilder, 
@@ -23,11 +24,12 @@ export class LoginServicesComponent implements OnInit {
     private loginservice: LoginService) { }
 
   ngOnInit() {
+       // this.message = "";
          this.loginservice.getLoginDetails('').subscribe(
                        response => {
-                            this.LoginDetails = response.recordset; console.log(JSON.stringify(response.recordset))
+                            this.LoginDetails = response.recordset; 
                               this.loginservice.getLoginStatusTypes().subscribe(
-                                  response => {this.LoginStatusTypes = response.recordset; console.log(JSON.stringify(response.recordset))},
+                                  response => {this.LoginStatusTypes = response.recordset;},
                                  error=>  {
                                     this.errorMessage = 'Unable to retrieve Login status types.' 
                                     console.log(this.errorMessage);
@@ -42,7 +44,8 @@ export class LoginServicesComponent implements OnInit {
 
   }
   LoginDetailsSearch()
-  {     
+  { 
+    this.message = "";    
      console.log(this.searchtext);
      this.loginservice.getLoginDetails(this.searchtext).subscribe(
                        response => {this.LoginDetails = response.recordset; console.log(JSON.stringify(response.recordset))},
@@ -51,16 +54,21 @@ export class LoginServicesComponent implements OnInit {
   }
   ApproveLoginDetails(loginDetails)
   {
-     console.log(JSON.stringify(loginDetails));
-     this.loginservice.approveLoginDetails(loginDetails.LoginId,loginDetails.Status,loginDetails.Broker_Search_Enabled).subscribe(
+    this.message = "";
+       if(confirm("Are you sure you want to approve login details?"))
+       {
+          this.loginservice.approveLoginDetails(loginDetails.LoginId,loginDetails.Status,loginDetails.Broker_Search_Enabled).subscribe(
                        response => {
                          this.loginservice.getLoginDetails(this.searchtext).subscribe(
-                          response => {this.LoginDetails = response.recordset; console.log(JSON.stringify(response.recordset))},
-                          error=>  { this.errorMessage = 'Unable to retrieve login details.' }
+                          response => {this.LoginDetails = response.recordset;
+                              this.message = 'Login details updated successfully.';
+                            },
+                          error=>  { this.errorMessage = 'Unable to retrieve login details.' ;  this.message = "";}
                            );  
                        },
-                          error=>  { this.errorMessage = 'Unable to retrieve login details.' }
-                      );       
+                          error=>  { this.errorMessage = 'Unable to retrieve login details.';  this.message = "";}
+                      );   
+       }    
   }
 
 }
